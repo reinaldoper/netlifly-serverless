079a34ac42bee614bd3da034ae173ce40cf9ffa4
reinaldoper <reinaldoper83@gmail.com>
2023-07-15 19:18:11 -0400

    feat: refactory: adiciona build

--- functions/goodbye.js
+++ functions/goodbye.js
@@ -1,7 +1,7 @@
 const handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello netlifly, World!' }),
+    body: JSON.stringify({ message: 'Hello netlifly aqui, World!' }),
   };
 };
 
