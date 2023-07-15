67b9e6c3b1cf5c556ad20b63aa08f4bef8285754
reinaldoper <reinaldoper83@gmail.com>
2023-07-15 18:51:10 -0400

    feat: adiciona funções do projeto

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,9 @@
-exports.handler = async (event, context) => {
+
+const handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello, World!' }),
   };
 };
+
+export { handler };
