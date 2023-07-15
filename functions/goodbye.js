ec279bed229cf0dcf04ea2dae98d62da04176525
reinaldoper <reinaldoper83@gmail.com>
2023-07-15 19:07:23 -0400

    refactory: adiciona build

--- functions/goodbye.js
+++ functions/goodbye.js
@@ -1,6 +1,8 @@
-exports.handler = async (event, context) => {
+const handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Goodbye, World!' }),
+    body: JSON.stringify({ message: 'Hello netlifly, World!' }),
   };
 };
+
+export { handler };
