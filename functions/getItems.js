236c229d76e42ae5f081e1ecd221023df0113577
reinaldoper <reinaldoper83@gmail.com>
2023-07-16 01:16:26 -0400

    feat: yyyy

--- functions/getItems.js
+++ functions/getItems.js
@@ -1,18 +1,30 @@
-// src/getItems.js
 import { prisma } from "./database.js";
-/* const cors = require("cors"); */
 
 const handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    // Resposta de preflight para o método OPTIONS
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://master--creative-alpaca-c36ba7.netlify.app',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET',
+      },
+      body: '',
+    };
+  }
+
+  // Lógica da sua função para o método GET
   const items = await prisma.employee.findMany();
+  
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "http://localhost:3000"
+      'Access-Control-Allow-Origin': 'https://master--creative-alpaca-c36ba7.netlify.app',
+      'Access-Control-Allow-Headers': 'Content-Type',
     },
     body: JSON.stringify(items),
   };
 };
 
 export { handler };
-
-
