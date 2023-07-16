eb083cd13525e3db4d2bf4b8cdbe8afe76f7e3bd
reinaldoper <reinaldoper83@gmail.com>
2023-07-16 18:19:34 -0400

    refactor: ajusta função calcular mes

--- functions/getTurnover.js
+++ functions/getTurnover.js
@@ -6,73 +6,84 @@ const handler = async (event) => {
   const verification = await prisma.employee.findUnique({
     where: { email: email },
   });
-  if (!verification){
+  if (!verification) {
     return {
       statusCode: 404,
       body: JSON.stringify({ error: "Email does not exist!" }),
     };
   }
-  
+
   try {
-    const gestorAtual = { email: email }; 
+    const gestorAtualEmail = email; 
+
     const employees = await prisma.employee.findMany();
     const headcounts = [];
-    
-    const currentDate = new Date();
-    // Iterar por cada mês
-    for (let month = 1; month <= 12; month++) {
+
+    // Extrair os meses com base na data de contratação (hireDate)
+    const monthsWithRecords = new Set();
+
+    employees.forEach(employee => {
+      if (employee.leaderEmail === gestorAtualEmail || employee.email === gestorAtualEmail) {
+        const hireDate = new Date(employee.hireDate);
+        const month = hireDate.getMonth() + 1; 
+        monthsWithRecords.add(month);
+      }
+    });
+
+    // Iterar apenas pelos meses com registros disponíveis
+    monthsWithRecords.forEach(async month => {
       const firstDayOfMonth = new Date(currentDate.getFullYear(), month - 1, 1);
       const lastDayOfMonth = new Date(currentDate.getFullYear(), month, 0);
 
       const employeesCountFirstDay = await prisma.employee.count({
         where: {
           OR: [
-            { leaderEmail: gestorAtual.email }, 
-            { email: gestorAtual.email } 
+            { leaderEmail: gestorAtualEmail },
+            { email: gestorAtualEmail }
           ],
           hireDate: { lte: firstDayOfMonth.toISOString().split('T')[0] },
           terminationDate: { lte: lastDayOfMonth.toISOString().split('T')[0] }
         },
       });
-      
+
       const headcountes = await prisma.employee.count({
         where: {
           OR: [
-            { leaderEmail: gestorAtual.email },
-            { email: gestorAtual.email } 
+            { leaderEmail: gestorAtualEmail },
+            { email: gestorAtualEmail }
           ],
           hireDate: { lte: lastDayOfMonth.toISOString().split('T')[0] },
           terminationDate: null,
         },
       });
 
-      const { turnover, headcountTotal } = calcularTurnoverEHeadcount(gestorAtual, employeesCountFirstDay, headcountes, employees);
+      const { turnover, headcountTotal } = calcularTurnoverEHeadcount(gestorAtualEmail, employeesCountFirstDay, headcountes, employees);
 
       headcounts.push({ month, turnover, headcountTotal });
-    }
+    });
 
     return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(headcounts),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(headcounts),
+  };
+} catch (error) {
+  return {
+    statusCode: 500,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ error: error.message }),
+  };
+}
 };
 
 export { handler };
