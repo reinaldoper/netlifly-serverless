8138818666ef8e418f84dd02d61b70d9ec0fb2ee
reinaldoper <reinaldoper83@gmail.com>
2023-07-16 02:04:06 -0400

    feat: ajuste

--- functions/getItems.js
+++ functions/getItems.js
@@ -1,27 +1,15 @@
 import { prisma } from "./database.js";
 
 const handler = async (event) => {
-  if (event.httpMethod === 'OPTIONS') {
-    // Resposta de preflight para o método OPTIONS
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET',
-      },
-      body: '',
-    };
-  }
-
-  // Lógica da sua função para o método GET
   const items = await prisma.employee.findMany();
   
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(items),
   };
