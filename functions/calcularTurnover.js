496a0f84ab9853605c839b25e29174ce0ff903ad
reinaldoper <reinaldoper83@gmail.com>
2023-07-18 16:00:23 -0400

    refactor: adiciona caso inativos vazio ao turnover

--- functions/calcularTurnover.js
+++ functions/calcularTurnover.js
@@ -2,17 +2,6 @@ const calcularTurnover = (datasAtivos, datasInativos, totalFuncionarios) => {
   const meses = [...new Set(datasAtivos.map(data => data.split('/')[1]))];
   const turnovers = [];
 
-  for (const mes of meses) {
-    const ativosMes = datasAtivos.filter(data => data.split('/')[1] === mes).length;
-    const inativosMes = datasInativos.filter(data => data.split('/')[1] === mes).length;
-
-    const turnover = inativosMes / ((ativosMes + totalFuncionarios) / 2);
-    turnovers.push({
-      turnover,
-      data: mes
-    });
-  }
-
   if (datasInativos.length === 0) {
     const ativosTotais = datasAtivos.length;
     const turnoverSemInativos = 0 / ((ativosTotais + totalFuncionarios) / 2);
@@ -20,6 +9,17 @@ const calcularTurnover = (datasAtivos, datasInativos, totalFuncionarios) => {
       turnover: turnoverSemInativos,
       data: null
     });
+  } else {
+    for (const mes of meses) {
+      const ativosMes = datasAtivos.filter(data => data.split('/')[1] === mes).length;
+      const inativosMes = datasInativos.filter(data => data.split('/')[1] === mes).length;
+
+      const turnover = inativosMes / ((ativosMes + totalFuncionarios) / 2);
+      turnovers.push({
+        turnover,
+        data: mes
+      });
+    }
   }
 
   turnovers.sort((a, b) => {
@@ -30,6 +30,7 @@ const calcularTurnover = (datasAtivos, datasInativos, totalFuncionarios) => {
 
   return turnovers;
 };
+
 
 
 export { calcularTurnover };
